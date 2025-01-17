b283d05e9fdfdb4b32c4f78db8eac3f0c8acb348
Momoy21 <oyendino@gmail.com>
2025-01-17 21:29:57 +0700

    Update vercel.json and ensure compatibility with PHP 8.2

--- config/cache.php
+++ config/cache.php
@@ -48,7 +48,7 @@ return [
 
         'file' => [
             'driver' => 'file',
-            'path' => storage_path('framework/cache/data'),
+            'path' => '/tmp/cache',
             'lock_path' => storage_path('framework/cache/data'),
         ],
 
