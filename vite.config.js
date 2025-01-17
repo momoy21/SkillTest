dc77b58e3a359a3049f5c3041aaade17f0a6ceb1
Angel <91896297+momoy21@users.noreply.github.com>
2025-01-17 16:09:40 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,7 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig(({ command }) => {
     return {
-        plugins: [
-            laravel({
-                input: ['resources/css/app.css', 'resources/js/app.js'],
-                refresh: true,
-            }),
-        ],
+        plugins: [],
         build: {
             outDir: command === 'build' ? 'dist' : 'public/build',
         },
